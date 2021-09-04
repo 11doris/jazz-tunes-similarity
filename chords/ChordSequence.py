54f3ef4da72669612e6b5668007e137dcdd69ba4
doris <11dzahnd@gmail.com>
2021-09-04 15:55:56 +0200

    Fill up chords per bar.

--- chords/ChordSequence.py
+++ chords/ChordSequence.py
@@ -26,10 +26,74 @@ class ChordSequence:
             data, names = self.data_obj.rootAndDegrees()
         return data
 
-    #def _get_key(self):
+    def fill_up_bar(self, _seq, _beats):
+        # get max number of chords per bar
+        max_chords = max([len(set(i)) for i in _seq])
+
+        # _seq is modified in this function!! create a copy of _seq and return this. Or change the test.
+
+        # fill up the chords to have the same number of chords in each bar
+        if max_chords == 1:
+            pass
+        elif max_chords == 2:
+            if _beats == 4:
+                for index, measure in enumerate(_seq):
+                    if len(measure) == 2:
+                        continue
+                    _seq[index].append(measure[0])
+            elif _beats == 3 or _beats == 6:
+                for index, measure in enumerate(_seq):
+                    if len(measure) == 1:
+                        new_seq = [measure[0]] * 3
+                        _seq[index] = new_seq
+                    elif len(measure) == 2:
+                        new_seq = [measure[0]] * 2
+                        new_seq.append(measure[1])
+                        _seq[index] = new_seq
+            elif _beats == 2:
+                for index, measure in enumerate(_seq):
+                    if len(measure) == 2:
+                        continue
+                    _seq[index].append(measure[0])
+            else:
+                raise NotImplementedError("Unsupported number of beats for 2 chords!")
+        elif max_chords == 4 or max_chords == 3:
+            if _beats == 4:
+                for index, measure in enumerate(_seq):
+                    if len(measure) == 4:
+                        continue
+                    if len(measure) == 1:
+                        new_seq = [measure[0]] * 4
+                        _seq[index] = new_seq
+                    elif len(measure) == 2:
+                        new_seq = [measure[0]] * 2
+                        new_seq.append(measure[1])
+                        new_seq.append(measure[1])
+                        _seq[index] = new_seq
+                    elif len(measure) == 3:
+                        new_seq = [measure[0]] * 2
+                        new_seq.append(measure[1])
+                        new_seq.append(measure[2])
+                        _seq[index] = new_seq
+            elif _beats == 3:
+                for index, measure in enumerate(_seq):
+                    if len(measure) == 3:
+                        continue
+                    elif len(measure) == 2:
+                        new_seq = [measure[0]] * 2
+                        new_seq.append(measure[1])
+                        _seq[index] = new_seq
+                    elif len(measure) == 1:
+                        new_seq = [measure[0]] * 3
+                        _seq[index] = new_seq
+            else:
+                raise NotImplementedError(f"Unsupported Beat with max_chords={max_chords}")
+        else:
+            raise NotImplementedError("Unsupported Max Number of Chords per Measure!")
+        return _seq
 
     def read_data(self):
-
+        # TODO: Move this Code!!
         directory = self.config['config']['output_directory']
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -45,7 +109,7 @@ class ChordSequence:
             tune = data[i]
             seq = []
             # initialize the chord sequence with an empty list per measure
-            for n in range(tune[len(tune)-1]['measure']):
+            for n in range(tune[len(tune) - 1]['measure']):
                 seq.append([])
 
             # transpose a major tune to C major, and a minor tune to A minor
@@ -58,18 +122,20 @@ class ChordSequence:
                 # replace mM9 chord by mM7 because it occurs only once
                 formatted_chord = re.sub('mM9$', 'mM7', formatted_chord)
                 # replace all maug chords; they occur only once minor-augmented =
-                seq[chord['measure']-1].append(formatted_chord)
+                seq[chord['measure'] - 1].append(formatted_chord)
                 # print("Bar {}: {}".format(chord['measure'], formatted_chord))
+
+            print(self.data_obj.meta[str(i)]['title'])
+            seq = self.fill_up_bar(seq, self.data_obj.meta[str(i)]['beat-time']['beats'])
             sequences += [seq]
 
-        assert(len(self.data_obj.meta) == len(sequences))
+        assert (len(self.data_obj.meta) == len(sequences))
 
         for _id, tune in enumerate(sequences):
-            #print(self.data_obj.meta[str(_id)]['title'])
-            #print(f'    {sequences[_id]}')
+            # print(self.data_obj.meta[str(_id)]['title'])
+            # print(f'    {sequences[_id]}')
             self.data_obj.meta[str(_id)]['out'] = {}
             self.data_obj.meta[str(_id)]['out']['chords'] = tune
-            self.data_obj.meta[str(_id)]['out']['duration'] = self.data_obj.duration[str(_id)]
-            assert(len(self.data_obj.meta[str(_id)]['out']['chords']) == len(self.data_obj.meta[str(_id)]['out']['chords']))
+            # self.data_obj.meta[str(_id)]['out']['duration'] = self.data_obj.duration[str(_id)]
 
         return self.data_obj.meta
