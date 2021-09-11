1aec96f458598eee87a0712866011124f67427d5
doris <11dzahnd@gmail.com>
2021-09-11 16:26:33 +0200

    Add file to separate the composers for each musicXML tune and extract the year information where available.

---
+++ data_preparation/iRealPro/01_explore_tunes.py
@@ -0,0 +1,52 @@
+import os
+import json
+import pandas as pd
+
+
+##
+# read meta info and convert to data frame
+meta = json.load(open('../../dataset/meta_info.json'))
+df = pd.DataFrame.from_dict(meta, orient='index')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index': 'file_name'})
+
+##
+#
+
+print(f'Found {len(df)} files to parse... ')
+
+df.groupby('composer').count()
+
+##
+# Cleaning
+
+# If the year is contained in the composer column, extract it to a new column
+df['year'] = df['composer'].str.extract(r"\(?(18[0-9]{2}|19[0-9]{2}|20[0-9]{2})\)?")
+df['composer'] = df['composer'].str.replace(r"\(?(18[0-9]{2}|19[0-9]{2}|20[0-9]{2})\)?", "", regex=True).str.strip()
+
+# Remove the (Dixieland Tunes) from the title
+df['title'] = df['title'].str.replace("(Dixieland Tunes)", "", regex=False).str.strip()
+df['title'] = df['title'].str.replace("(dixieland Tunes)", "", regex=False).str.strip()
+
+# Fix composer names
+df['composer'] = df['composer'].str.replace("Van-Heusen", "Van Heusen", regex=False)
+df['composer'] = df['composer'].str.replace("Armstroong", "Louis Armstrong", regex=False)
+df['composer'] = df['composer'].str.replace("Antonio-Carlos", "Antonio Carlos", regex=False)
+
+
+# Split multiple composers to multiple rows
+df = df.assign(composer=df.composer.str.split(',')).explode('composer')
+df = df.assign(composer=df.composer.str.split('-')).explode('composer')
+df['composer'] = df['composer'].str.strip()
+
+##
+#
+composer_tunes = df.groupby('composer').title.count().sort_values(ascending=False)
+composer_tunes.head(50)
+
+
+##
+#
+df.reset_index(inplace=True)
+df.to_csv('ireal_tunes_composer.csv', sep='\t', index=False)
+
