f5c36de42067469e7b22881eac2ec420d156d8b6
doris <11dzahnd@gmail.com>
2021-12-03 13:42:04 +0100

    Save data for both chord preprocessing methods.

--- 03b_input_wordembedding.py
+++ 03b_input_wordembedding.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     cs = ChordSequence()
 
     # get data including section info
-    df = cs.split_tunes_in_sections()
-    df.to_csv('03b_input_wordembedding_sections.csv', sep='\t', encoding='utf-8', index=True, index_label="id")
+    cs.split_tunes_in_sections(chords='rootAndDegreesPlus')
+    cs.split_tunes_in_sections(chords='simplified')
 
     # # get data for tunes
     # df = cs.get_tunes_data()
