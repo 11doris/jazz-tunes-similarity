a7223410d86ea3fa8b65b157eeac92b2eace7e46
doris <11dzahnd@gmail.com>
2021-12-20 13:50:56 +0100

    Leadsheet: start a new line if section starts in the middle of a line.

--- 99_leadsheet.py
+++ 99_leadsheet.py
@@ -30,15 +30,14 @@ def get_html_chord_format(row, relative=True):
     ),
     '''
 
-
 def generate_html_leadsheet(df: pd.DataFrame, relative=True):
-    last_measure = 1
+    last_measure_absolut = 1
+    last_measure_on_line = 1
     last_beat = 0
     bar = ""
     lines = ""
-    html_str = ""
 
-    debug = True
+    debug = False
 
     # Add first section label
     section_label = df['SectionLabel'].iloc[0] if df['StartOfSection'].iloc[0] else ""
@@ -50,48 +49,62 @@ def generate_html_leadsheet(df: pd.DataFrame, relative=True):
             print(row['SectionLabel'], row['StartOfSection'])
             print(row['MeasureNum'], row['Beat'])
 
-        if row['MeasureNum'] == last_measure:
+        # we're still handling chords in the same measure
+        if row['MeasureNum'] == last_measure_absolut:
             while row["Beat"] - 1 > last_beat:
                 last_beat += 1
                 bar += f'html.Div("", className="beat b{last_beat}"),'
 
             bar += f'html.Div({get_html_chord_format(row, relative)} className="beat b{row["Beat"]}"),'
             last_beat += 1
-            if debug:
-                print("Bar: ", bar)
 
-        elif row['MeasureNum'] != last_measure:
+        # now we're in a new measure
+        elif row['MeasureNum'] != last_measure_absolut:
             # finish the previous measure
-            measure_num = last_measure % 4
+            measure_num = last_measure_on_line % 4
             measure_num = 4 if measure_num == 0 else measure_num
+
+            # increase the counter which counts the bar number on the current line
+            last_measure_on_line += 1
+
             m = f'html.Div(html.Div([{bar}],className="beat-wrapper",),className="measure m{measure_num}",),'
-            if debug:
-                print(m)
             lines += m
             last_beat = 1
 
-            # if the previous measure was measure 4, add the section label next
-            if measure_num == 4:
+            # Add the section label next if we're at the last measure or a new section starts
+            if (measure_num == 4) or (row['StartOfSection']):
+                s = ""
+                # if a new section starts and we're not at the end of the line, fill up the line with empty bars
+                if (measure_num < 4) and (row['StartOfSection']):
+                    # if a new section starts before measure 4, start a new line
+                    s += f'html.Div("",className="left-double-bar"),'
+                    for i in range(measure_num+1, 4):
+                        s += f'html.Div("",className="section"),'
+
                 section_label = row['SectionLabel'] if row['StartOfSection'] else ""
-                s = f'html.Div("{section_label}", className="section"),'
+                s += f'html.Div("{section_label}", className="section"),'
+
                 lines += s
+                last_measure_on_line = 1
 
             # get the chord of the current measure and bar
             bar = ""
             bar += f'html.Div({get_html_chord_format(row, relative)} className="beat b{row["Beat"]}"),'
-            last_measure = row["MeasureNum"]
+            last_measure_absolut = row["MeasureNum"]
+
+            if debug:
+                print(f'last_abs: {last_measure_absolut}, last_rel: {last_measure_on_line}')
 
     # finish the last bar
-    if row['MeasureNum'] == last_measure:
-        measure_num = last_measure % 4
+    if row['MeasureNum'] == last_measure_absolut:
+        measure_num = last_measure_absolut % 4
         measure_num = 4 if measure_num == 0 else measure_num
         m = f'html.Div(html.Div([{bar}],className="beat-wrapper",),className="measure m{measure_num}",),'
         #    print(m)
         lines += m
 
-    html_str = f'html.Div([{lines}],className="wrapper",),'
+    return f'html.Div([{lines}],className="wrapper",),'
 
-    return html_str
 
 
 ##
