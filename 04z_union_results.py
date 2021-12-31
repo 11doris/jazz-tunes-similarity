edf8e9a7be2b9cc2d95e592de7e8b8fc0c779ec3
doris <11dzahnd@gmail.com>
2021-12-31 19:13:19 +0100

    Rework to find the common and unique recommendations for two different models. Output is used for webapp.

--- 04z_union_results.py
+++ 04z_union_results.py
@@ -1,75 +1,113 @@
 import pandas as pd
 from dataset.utils import set_pandas_display_options
 import matplotlib.pyplot as plt
-from tqdm import tqdm
+import zipfile
 
-set_pandas_display_options()
 
-df_simp = pd.read_csv('output/similarity/recommender_lsi_simp.zip')
-df_plus = pd.read_csv('output/similarity/recommender_lsi_plus.zip')
+if __name__ == "__main__":
 
-df_simp.head()
+    set_pandas_display_options()
 
-ref_titles = (df_simp
-              .loc[:, ['reference_titleid', 'ref_section_label']]
-              .drop_duplicates()
+    input_files = [
+        'output/similarity/recommender_lsi_simp.zip',
+        'output/similarity/recommender_lsi_plus.zip'
+    ]
+
+    # read all input csv files into a dataframe and append a new column with the filename
+    # df_from_each_file = (pd.read_csv(f).assign(filename = f.split('/')[-1].split('.')[0]) for f in input_files)
+    # df = pd.concat(df_from_each_file, ignore_index=True)
+
+    df_list = []
+    for f in input_files:
+        temp_df = pd.read_csv(f)
+        temp_df['method'] = f.split('/')[-1].split('.')[0]
+        df_list.append(temp_df)
+
+    df_list
+
+    ref_titles = (df_list[0]
+                  .loc[:, ['reference_titleid', 'ref_section_label']]
+                  .drop_duplicates()
+                  )
+
+    # find the common tunes for all recommendation methods
+
+    len_common = []
+
+    dict_comparison = {
+        'titleid': [],
+        'section_label': [],
+        'common': [],
+        'unique_simp': [],
+        'unique_plus': [],
+    }
+
+    common = (df_list[0]
+              .merge(df_list[1], on=['reference_title',
+                                     'reference_titleid',
+                                     'ref_section_label',
+                                     'ref_sectionid',
+                                     'similar_titleid',
+                                     'similar_title',
+                                     'similar_sectionid',
+                                     'similar_section_label'])
+              )
+    common['common'] = True
+    common['score'] = common[['score_x', 'score_y']].max(axis=1)
+    common['method'] = 'common'
+
+    common.drop(['score_x', 'score_y', 'method_x', 'method_y', 'index_x', 'index_y'], inplace=True, axis=1)
+
+    unique = (df_list[0]
+              .merge(df_list[1], on=['reference_title',
+                                     'reference_titleid',
+                                     'ref_section_label',
+                                     'ref_sectionid',
+                                     'similar_titleid',
+                                     'similar_title',
+                                     'similar_sectionid',
+                                     'similar_section_label'], how='outer', indicator=True)
+              .query('_merge != "both"')
+              .drop('_merge', 1)
               )
 
-df_simp.value_counts(['reference_titleid', 'ref_section_label'])
+    unique['score_x'].fillna(0.0, inplace=True)
+    unique['score_y'].fillna(0.0, inplace=True)
+    unique['method_x'].fillna("", inplace=True)
+    unique['method_y'].fillna("", inplace=True)
+    unique['score'] = unique['score_x'] + unique['score_y']
+    unique['method'] = unique['method_x'] + unique['method_y']
+    unique.drop(['score_x', 'score_y', 'method_x', 'method_y', 'index_x', 'index_y'], inplace=True, axis=1)
+    unique = unique.sort_values(['ref_sectionid', 'score'], ascending=[True, False])
+    unique['common'] = False
 
-len_common = []
+    df = pd.concat([common, unique])
+    df = df.sort_values(['ref_sectionid', 'common', 'score'], ascending=[True, False, False])
+    df['method'] = df['method'].str.replace('recommender_lsi_simp', 'simplified')
+    df['method'] = df['method'].str.replace('recommender_lsi_plus', 'basic')
 
-dict_comparison = {
-    'titleid': [],
-    'section_label': [],
-    'common': [],
-    'unique_simp': [],
-    'unique_plus': [],
-}
+    common_count = common.value_counts(['reference_titleid', 'ref_section_label'])
+    unique_count = unique.value_counts(['reference_titleid', 'ref_section_label'])
 
-for section in tqdm(ref_titles.iterrows()):
-    titleid = section[1]['reference_titleid']
-    section = section[1]['ref_section_label']
-    q = f"reference_titleid == {titleid} & ref_section_label == '{section}'"
-    simp_tunes = df_simp.query(q)
-    plus_tunes = df_plus.query(q)
+    ax1 = plt.subplot(1, 2, 1)
+    plt.hist(common_count, bins=30)
+    ax1.set_title('Common')
 
-    recommend_simp = set(list(zip(simp_tunes.similar_titleid, simp_tunes.similar_section_label)))
-    recommend_plus = set(list(zip(plus_tunes.similar_titleid, plus_tunes.similar_section_label)))
+    ax2 = plt.subplot(1, 2, 2, sharex=ax1)
+    plt.hist(unique_count, bins=30)
+    ax2.set_title('Unique')
 
-    common_set = recommend_plus.intersection(recommend_simp)
-    unique_simp = len(recommend_simp) - len(common_set)
-    unique_plus = len(recommend_plus) - len(common_set)
-    assert(unique_simp == unique_plus)
-
-    if False:
-        print(f'Len Simp: {len(recommend_simp)}')
-        print(f'Len Plus: {len(recommend_plus)}')
-        print(f'Len Common: {len(common_set)}')
-        print()
-
-    # ['titleid', 'section_label', 'common', 'unique_simp', 'unique_plus']
-    row = [titleid, section, len(common_set), unique_simp, unique_plus]
-
-    dict_comparison['titleid'].append(titleid)
-    dict_comparison['section_label'].append(section)
-    dict_comparison['common'].append(len(common_set))
-    dict_comparison['unique_simp'].append(unique_simp)
-    dict_comparison['unique_plus'].append(unique_plus)
+    plt.show()
 
 
-df = pd.DataFrame(dict_comparison)
-
-
-
-# equivalent but more general
-ax1 = plt.subplot(1, 2, 1)
-plt.hist(df['common'], bins=30)
-ax1.set_title('Common')
-
-ax2 = plt.subplot(1, 2, 2, sharex=ax1)
-plt.hist(df['unique_plus'], bins=30)
-ax2.set_title('Unique')
-
-plt.show()
-
+    # reference_titleid, similar_titleid, ref_section_label, similar_section_label, score
+    (df[['reference_titleid',
+         'similar_titleid',
+         'ref_section_label',
+         'similar_section_label',
+         'method',
+         'score']]
+     .to_csv('output/model/unified_model_result.csv')
+     )
+    with zipfile.ZipFile(f'output/model/unified_model_result.zip', 'w') as zf:
+        zf.write(f'output/model/unified_model_result.csv')
