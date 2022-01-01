b37166af5a7bed040376558d5ad4965b59f35409
doris <11dzahnd@gmail.com>
2022-01-01 22:43:35 +0100

    Work in progress to visualize LSI vectors for a tune.

--- 04b_visualize_space.py
+++ 04b_visualize_space.py
@@ -16,49 +16,6 @@ from gensim.models.lsimodel import LsiModel
 
 if __name__ == "__main__":
     set_pandas_display_options()
-    df_vectors = pd.read_csv('output/model/weights_tunes_vectors.csv')
-
-    # print rows with nan or inf values
-    invalid = df_vectors[df_vectors.isin([np.nan, np.inf, -np.inf]).any(1)]
-    print(invalid)
-    assert(len(invalid) == 0)
-
-    #df_vectors = df_vectors[~df_vectors.isin([np.nan, np.inf, -np.inf]).any(1)]
-    tune_legend = pd.read_csv('output/model/weights_tune_vectors_legend.csv')
-
-    assert(len(df_vectors) == len(tune_legend))
-
-    # Visualize LSI Weights with PCA
-    if False:
-        data = df_vectors.values.tolist()
-
-        # scale to 0..1
-        scaled_data = MinMaxScaler().fit_transform(data)
-        # max(map(max, scaled_data))
-        # min(map(min, scaled_data))
-
-        pca = PCA(n_components=3)
-
-        # Create the transformation model for this data. Internally, it gets the rotation matrix and the explained variance
-        pcaTr = pca.fit(scaled_data)
-        rotatedData = pcaTr.transform(scaled_data)  # Transform the data base on the rotation matrix of pcaTr
-
-        # # Create a data frame with the new variables. We call these new variables PC1 and PC2
-        dataPCA = pd.DataFrame(data=rotatedData, columns=['PC1', 'PC2', 'PC3'])
-        dataPCA['title'] = tune_legend
-        projected = pd.DataFrame(dataPCA)
-
-        fig = px.scatter_3d(
-            dataPCA,
-            x='PC1', y='PC2', z='PC3',
-            hover_name='title',
-            opacity=0.2,
-            #width=500, height=400
-        )
-        fig.update_traces(textposition='top center')
-        fig.update_traces(textfont_size=10, selector=dict(type='scatter'))
-        fig.show()
-
 
     # Load the LSI Model
     prep = CalculateLsiModel('rootAndDegreesPlus')
@@ -66,12 +23,80 @@ if __name__ == "__main__":
     prep.load_model()
     prep.load_similarity_matrix()
 
-    id = prep.title_to_sectionid_unique_section('These Foolish Things [jazz1350]')[1]
-    sims = prep.get_similar_tunes(id)
+    #
+    df_vectors = prep.get_tune_vectors()
 
+    # print rows with nan or inf values
+    invalid = df_vectors[df_vectors.isin([np.nan, np.inf, -np.inf]).any(1)]
+    print(invalid)
+    assert(len(invalid) == 0)
+
+    #df_vectors = df_vectors[~df_vectors.isin([np.nan, np.inf, -np.inf]).any(1)]
+
+
+    ###
+    title_name = 'These Foolish Things [jazz1350]'
+    sectionid = prep.title_to_sectionid_unique_section(title_name)[1]
+    sims = prep.get_similar_tunes(sectionid, 10)
+
+    tunes = []
     for id, score in sims:
         s2 = prep.row_id_to_sectionid(id)
-        print(s2, prep.sectionid_to_title(s2), score)
+        print(s2, id, prep.sectionid_to_title(s2), score)
+        tunes.append(id)
 
+    df_sim = pd.DataFrame(sims).set_index(0)
+
+
+    ##
+    # Visualize LSI Weights with PCA
+    if True:
+        data = df_vectors.values.tolist()
+
+        # scale to 0..1
+        scaled_data = MinMaxScaler().fit_transform(data)
+        # max(map(max, scaled_data))
+        # min(map(min, scaled_data))
+
+        pca = PCA(n_components=2)
+
+        # Create the transformation model for this data. Internally, it gets the rotation matrix and the explained variance
+        pcaTr = pca.fit(scaled_data)
+        rotatedData = pcaTr.transform(scaled_data)  # Transform the data base on the rotation matrix of pcaTr
+
+        # # Create a data frame with the new variables. We call these new variables PC1 and PC2
+        dataPCA = pd.DataFrame(data=rotatedData, columns=['PC1', 'PC2'])
+        dataPCA['title'] = list(prep.df_section['title_playlist'])
+        dataPCA['reference'] = 'similar'
+        dataPCA.at[prep.sectionid_to_row_id(sectionid), 'reference'] = 'reference'
+        dataPCA['size'] = 1.5
+        dataPCA.at[prep.sectionid_to_row_id(sectionid), 'size'] = 10.0
+        dataPCA['score']= 1.0
+        dataPCA.at[tunes, 'score'] = df_sim.iloc[:,0]
+
+        tunes.append(prep.sectionid_to_row_id(sectionid))
+
+
+        fig = px.scatter(
+            dataPCA.iloc[tunes],
+            x='PC1', y='PC2',
+            title=f'{title_name}',
+            hover_name='title',
+            color='score',
+            symbol='reference',
+            symbol_sequence={0:'circle', 1:'x'},
+            size='size',
+            size_max=12,
+            color_continuous_scale='RdBu',
+            color_continuous_midpoint=0.5,
+            width=500, height=400
+        )
+        fig.update_traces(marker=dict(line=dict(width=0,
+                                                color=None)),
+                          selector=dict(mode='markers'))
+        fig.update_layout(coloraxis_showscale=False)
+        #fig.update_traces(textposition='top center')
+        #fig.update_traces(textfont_size=10, selector=dict(type='scatter'))
+        fig.show()
 
 
