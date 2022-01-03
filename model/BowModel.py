e5c5c5ebe69a31d0c1307a46510239c0c300b0d7
doris <86537242+11doris@users.noreply.github.com>
2022-01-03 11:55:41 +0100

    Calculate similarities for tune sections based on LSI Model. (#24)
    * Add code from colab, converted from ipynb to py.
    
    * Code transferred from colab notebook for LSI model (without any visualization).
    
    * finish wandb at the end of the run.
    
    * Add a comment; had a mistake in the model, but it performed better....
    
    * Rework code to include only the first section if there are more than one with same label.
    
    Work in progress.
    
    * Generate test data for web application.
    
    * Massive speedup: store recommender results to dictionary and then convert to dataframe. Rearrange columns.
    
    * Trial to find out how many common matches for the Plus vs Simplified chord vocabulary.
    
    * Rework to find the common and unique recommendations for two different models. Output is used for webapp.
    
    * Work in progress to visualize vectors of one similar tune.
    
    * Put data preprocessing in separate function.
    
    * Add methods to store and load the model and index.
    
    * Add methods to store and load the model and index.
    
    * Add function to get the LSI vectors for tunes. Add function to get a list of similar tunes for one tune.
    
    * Work in progress to visualize LSI vectors for a tune.
    
    * Display PCA scatter plot with the most relevant and irrelevant tunes.
    
    * Visualize Relevance feedback for one tune.
    - Original query
    - Using one positive feedback
    - Using centroid of relevant tunes.
    
    * Cleanup and add comments.
    
    * Clean up the mess between train and test set.
    
    * Clean up the mess between train and test set, part  2.
    
    * Clean up the mess between train and test set, part 3, make contrafacts test work again.
    
    * Provide a dataframe which concats the train and test data for simpler use.
    
    * Clean up warnings.
    
    * Fix: add the test documents before saving the model.
    
    * Calculate the LSI topics for the full dataset.
    
    * Cleanup.
    
    * For PCA, scale the input data to unit variance.
    
    * Add comments.
    
    * Append the preprocessing method to the output files.
    
    * Get number of results from config.
    
    * Increase the no_below value to reduce the size of dictionary.
    
    * Adapt filenames.
    
    * Rename files.
    
    * Run model for both chord preprocessing methods.
    
    * Cleanup.
    
    * Move preprocessing config parameters into a dict.
    
    * Cleanup comments.

---
+++ model/BowModel.py
@@ -0,0 +1,153 @@
+import pandas as pd
+from tqdm import tqdm
+from model.PrepareData import PrepareData
+from model.config import get_test_tunes, preprocess_config
+from gensim import corpora
+
+
+class BowModel(PrepareData):
+    pass
+
+    def prepare_dict_and_corpus(self, df_clean):
+        """
+        Input  : cleaned tunes
+        Purpose: create term dictionary of our courpus and Converting list of documents (corpus) into Document Term Matrix
+        Output : term dictionary and Document Term Matrix
+        """
+        doc_clean = list(df_clean['chords'])
+        # Creating the term dictionary of our courpus, where every unique term is assigned an index.
+        dic = corpora.Dictionary(doc_clean)
+        # Filter out words that occur only in a few documents.
+        dic.filter_extremes(no_below=preprocess_config['no_below'], no_above=1.0)
+
+        # Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
+        doc_term_matrix = [dic.doc2bow(text) for text in doc_clean]
+
+        # generate LDA model
+        return dic, doc_term_matrix
+
+    def prepare_corpus(self, df_clean, dic):
+        doc_clean = list(df_clean['chords'])
+        doc_term_matrix = [dic.doc2bow(text) for text in doc_clean]
+        return doc_term_matrix
+
+    def get_sim_scores(self, topn=preprocess_config['test_topN']):
+
+        dict_sim = {
+            'reference_title': [],
+            'reference_titleid': [],
+            'ref_sectionid': [],
+            'ref_section': [],
+            'ref_section_label': [],
+            'similar_title': [],
+            'similar_titleid': [],
+            'similar_sectionid': [],
+            'similar_section': [],
+            'similar_section_label': [],
+            'score': [],
+        }
+
+        tunes = list(self.tunes['title_playlist'].values())
+
+        # # for debugging only
+        # tunes = [
+        #     "Sweet Sue, Just You [jazz1350]",
+        #     "On The Sunny Side Of The Street [jazz1350]",
+        #     "These Foolish Things [jazz1350]",
+        #     "Blue Moon [jazz1350]",
+        #     "I Got Rhythm [jazz1350]",
+        # ]
+
+        for tune in tqdm(tunes):
+            for s1 in self.title_to_sectionid_unique_section(tune):
+                query = self.df_train_test.loc[s1]['chords']
+                query_bow = self.train_dictionary.doc2bow(query)
+
+                # perform a similarity query against the corpus
+                similarities = self.index_lsi[self.lsi[query_bow]]
+                sims = sorted(enumerate(similarities), key=lambda item: -item[1])
+
+                n = 0
+                for id, s2_score in sims:
+                    # store the top N best results
+                    if n >= topn:
+                        break
+
+                    s2 = self.get_train_test_sectionid(id)
+                    # don't count self-similarity between sections of the same tune
+                    if s2 not in self.title_to_sectionid(tune):
+                        # print(f"\t{s2_score:.3f} {sectionid_to_section[s2]}")
+                        n += 1
+
+                        dict_sim['reference_title'].append(tune)
+                        dict_sim['reference_titleid'].append(self.title_to_titleid(tune))
+                        dict_sim['ref_sectionid'].append(s1)
+                        dict_sim['ref_section'].append(self.sectionid_to_section(s1))
+                        dict_sim['ref_section_label'].append(self.sectionid_to_sectionlabel(s1))
+                        dict_sim['similar_title'].append(self.sectionid_to_title(s2))
+                        dict_sim['similar_titleid'].append(self.sectionid_to_titleid(s2))
+                        dict_sim['similar_sectionid'].append(s2)
+                        dict_sim['similar_section'].append(self.sectionid_to_section(s2))
+                        dict_sim['similar_section_label'].append(self.sectionid_to_sectionlabel(s2))
+                        dict_sim['score'].append(s2_score)
+
+        df_sim = pd.DataFrame.from_dict(dict_sim)
+
+        return df_sim
+
+    def test_contrafacts(self, model, index, n=15):
+        matches = 0
+        results = {}
+
+        for tune, similar_tune in get_test_tunes():
+
+            # loop over all sections of the tune
+            section_matches = 0
+            rank = 0
+            score = 0
+            for s1 in self.title_to_sectionid_unique_section(tune):
+                query = self.df_test.loc[s1]['chords']
+                query_bow = self.train_dictionary.doc2bow(query)
+
+                # perform a similarity query against the corpus
+                similarities = index[model[query_bow]]
+                sims = sorted(enumerate(similarities), key=lambda item: -item[1])
+                # print(f"{tune} s1: {s1}")
+                # print(f"similar_tune: {similar_tune}, {title_to_sectionid[similar_tune]}")
+                # print(sims)
+                # print(len(sims))
+
+                # check if the section matches the expected title; consider only the first N recommendations
+                i = 0
+
+                for id, value in sims:
+                    if i >= n:
+                        break
+                    sectionid = self.get_train_test_sectionid(id)
+                    if self.sectionid_to_title(sectionid) == similar_tune:
+                        section_matches += 1
+                        print(
+                            f"Found {tune} - {similar_tune} {self.sectionid_to_sectionlabel(sectionid)} with value {value}")
+                        if value > score:
+                            rank = i
+                            score = value
+                        break
+                    i += 1
+
+            # for each title, increase matches if at least one of the section matched the expected title
+            if section_matches > 0:
+                matches += 1
+                results[f'{tune}, {similar_tune}'] = {'found': 1,
+                                                      'score': score,
+                                                      'rank': rank}
+            else:
+                score = 0
+                for sectionid in self.title_to_sectionid_unique_section(similar_tune):
+                    sim_value = similarities[self.df_train_test.loc[sectionid]['index']]
+                    score = sim_value if sim_value > score else score
+
+                results[f'{tune}, {similar_tune}'] = {'found': 0,
+                                                      'score': score,
+                                                      'rank': rank}
+
+        return matches, results
---
+++ model/PrepareData.py
@@ -0,0 +1,200 @@
+from model.config import input_files, get_test_tunes, preprocess_config
+import pandas as pd
+import logging
+import os
+
+
+def _remove_chord_repetitions(chords):
+    previous = ''
+    chords_norep = []
+    for c in chords:
+        if c != previous:
+            chords_norep.append(c)
+            previous = c
+    return chords_norep
+
+
+def _make_ngrams(tokens, n=2, sep='-'):
+    return [sep.join(ngram) for ngram in zip(*[tokens[i:] for i in range(n)])]
+
+
+def _get_list_of_test_tunes():
+    test_tunes = []
+    for ref, sim in get_test_tunes():
+        if ref not in test_tunes:
+            test_tunes.append(ref)
+        if sim not in test_tunes:
+            test_tunes.append(sim)
+    test_tunes.sort()
+    return test_tunes
+
+
+class PrepareData:
+    def __init__(self, chords_preprocessing='rootAndDegreesPlus'):
+        """
+        Reads the input data and stores it into dataframes:
+        self.df: read from the csv. dataframe with full information; one per section and tune, same sections per tune
+                 are retained. Includes the metadata with title, tune mode etc.
+        self.df_section: if multiple sections with the same label for a tune, keep only the first section.
+
+        Provides helper functions to translate between title and sectionid, and rowid
+
+        :param chords_preprocessing:
+        """
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+        self.chords_preprocessing = chords_preprocessing
+        self.ngrams = preprocess_config['ngrams']
+        self.remove_repetitions = preprocess_config['remove_repetitions']
+
+        self.input_file = input_files[chords_preprocessing]
+
+        self.df = pd.read_csv(self.input_file, sep='\t', index_col="id")
+        self.df = self.df.reset_index()
+
+        # if multiple sections with same label, reduce dataset to just one
+        self.df_section = (self.df
+                               .sort_values(['tune_id', 'section_name', 'section_id'])
+                               .drop_duplicates(['tune_id', 'section_name'], keep='first')
+                               .loc[:, ['id', 'chords', 'title_playlist']]
+                               )
+        self.df_section.rename(columns={'id': 'sectionid'}, inplace=True)
+        self.df_section['chords'] = self.df_section['chords'].str.split(' ')
+        self.df_section.reset_index(inplace=True, drop=True)
+
+        ##
+        # Helper functions to translate between titles and sections
+        titles = self.df.loc[:, ['id', 'tune_id', 'section_id', 'section_name', 'title', 'title_playlist', 'tune_mode']]
+        self._titles_dict = titles.to_dict()
+
+        tunes = self.df.loc[:, ['tune_id', 'title_playlist']].drop_duplicates()
+        self.tunes = tunes.set_index('tune_id').to_dict()
+        titles_rows = titles.to_dict(orient='records')
+
+        # given the sectionid, provide a string with the title and the section label
+        self._sectionid_to_section = []
+        for i, row in enumerate(titles_rows):
+            name = f"{row['title']}, section{row['section_id']} ({row['section_name']})"
+            self._sectionid_to_section.append(name)
+
+        # given the sectionid, return the name of the section, e.g. 'A', 'B', 'C'...
+        self._sectionid_to_sectionlabel = []
+        for i, row in enumerate(titles_rows):
+            self._sectionid_to_sectionlabel.append(row['section_name'])
+
+        # given the title, provide a list of all corresponding sectionids (including duplicate sections)
+        self._title_to_sectionid = {}
+        for row in titles.iterrows():
+            title = row[1]['title_playlist']
+            if title not in self._title_to_sectionid:
+                self._title_to_sectionid[title] = [row[1]['id']]
+            else:
+                self._title_to_sectionid[title].append(row[1]['id'])
+
+        # list with the sectionid of the sections considered for training (omitting duplicate sections per tune)
+        self._title_to_sectionid_unique_section = {}
+        for row in self.df_section.iterrows():
+            title = row[1]['title_playlist']
+            if title not in self._title_to_sectionid_unique_section:
+                self._title_to_sectionid_unique_section[title] = [row[1]['sectionid']]
+            else:
+                self._title_to_sectionid_unique_section[title].append(row[1]['sectionid'])
+
+        # prepare a list of all sectionids under test
+        self.test_tune_sectionid = []
+        for title in _get_list_of_test_tunes():
+            self.test_tune_sectionid.extend(self.title_to_sectionid_unique_section(title))
+
+        # make sure that the directories for storing output files exist
+        if not os.path.exists('output'):
+            os.makedirs('output')
+        if not os.path.exists('output/model'):
+            os.makedirs('output/model')
+
+        # load the Corpus
+        self.__corpus()
+
+        ###
+    # public helper functions
+    def sectionid_to_title(self, id):
+        return self._titles_dict['title_playlist'][id]
+
+    def sectionid_to_titleid(self, id):
+        return self._titles_dict['tune_id'][id]
+
+    def titleid_to_title(self, id):
+        return self.tunes['title_playlist'][id]
+
+    def title_to_titleid(self, id):
+        titleid_dict = {v: k for k, v in self.tunes['title_playlist'].items()}
+        return titleid_dict[id]
+
+    def sectionid_to_section(self, id):
+        return self._sectionid_to_section[id]
+
+    def sectionid_to_sectionlabel(self, id):
+        return self._sectionid_to_sectionlabel[id]
+
+    def title_to_sectionid(self, id):
+        return self._title_to_sectionid[id]
+
+    def title_to_sectionid_unique_section(self, title):
+        return self._title_to_sectionid_unique_section[title]
+
+    ###
+    # Corpus processing
+
+    # depending on the configuration, remove subsequent identical chords or add ngrams of the chords.
+    def preprocess_input(self, chord_list):
+        tune_n = []
+        if self.remove_repetitions:
+            chord_list = _remove_chord_repetitions(chord_list)
+        for n in self.ngrams:
+            tune_n.extend(_make_ngrams(chord_list, n=n))
+        return tune_n
+
+    # process the input data, which is the unique sections of the tunes
+    def __corpus(self):
+        self.df_test = pd.DataFrame(columns=['sectionid', 'chords'])
+        self.df_train = pd.DataFrame(columns=['sectionid', 'chords'])
+
+        test_corpus_chords = []
+        train_corpus_chords = []
+
+        test_sectionid = []
+        train_sectionid = []
+
+        # for each unique section of a tune, process the chords
+        for id, line in self.df_section.iterrows():
+            sectionid = line['sectionid']
+            tune_n = self.preprocess_input(line['chords'])
+
+            # to the train_corpus, add only the sections which are not used by the contrafacts tests
+            if sectionid in self.test_tune_sectionid:
+                test_corpus_chords.append(tune_n)
+                test_sectionid.append(sectionid)
+            else:
+                train_corpus_chords.append(tune_n)
+                train_sectionid.append(sectionid)
+
+        self.df_test = pd.DataFrame(list(zip(test_sectionid, test_corpus_chords)),
+                                    columns=['sectionid', 'chords'])
+        self.df_test.set_index('sectionid', inplace=True)
+
+        self.df_train = pd.DataFrame(list(zip(train_sectionid, train_corpus_chords)),
+                                     columns=['sectionid', 'chords'])
+        self.df_train.set_index('sectionid', inplace=True)
+
+        self.df_train_test = pd.concat([self.df_train, self.df_test]).reset_index().reset_index().set_index('sectionid')
+
+        print(f'Train Corpus: {len(self.df_train)}')
+        print(f'Test Corpus: {len(self.df_test)}')
+
+    def get_test_corpus(self):
+        return self.df_test
+
+    def get_train_corpus(self):
+        return self.df_train
+
+    def get_train_test_sectionid(self, id):
+        return self.df_train_test.iloc[id].name
