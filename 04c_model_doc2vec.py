5e46b77363a1999eccf09e9e6820c47639734c13
doris <11dzahnd@gmail.com>
2022-01-10 10:39:08 +0100

    Remove all trials and leave only the model generation and contrafacts test here.

--- 04c_model_doc2vec.py
+++ 04c_model_doc2vec.py
@@ -21,10 +21,10 @@ def do_contrafacts_test(doc2VecObject):
     print()
     print(f"Found matches: {matches} out of {len(results)}: {100 * matches / len(results):.3f}%")
 
-    wandb.store_results(doc2VecObject.model_name, matches, df_sim, doc2VecObject.model_config)
+    wandb.store_results(doc2VecObject.model_name, matches, df_sim)
 
 
-def diatonic_chords(doc2vecObj, preprocessing):
+def similar_chords(doc2vecObj, preprocessing):
 
     model = doc2vecObj.doc2vec
     if 1 in preprocess_config['ngrams']:
@@ -36,75 +36,6 @@ def diatonic_chords(doc2vecObj, preprocessing):
         print(f"\nMost similar to {ref}:")
         for t in model.wv.similar_by_word(ref, topn=20):
             print(t)
-
-def corpus_chord_ngram(doc2vecObj, ngrams):
-    _df = pd.DataFrame(columns=['sectionid', 'chords'])
-    list_corpus_chords = []
-    list_sectionid = []
-
-    # for each unique section of a tune, process the chords
-    for id, line in doc2vecObj.df_section.iterrows():
-        sectionid = line['sectionid']
-        tune_n = doc2vecObj.preprocess_input(line['chords'], ngrams=ngrams)
-
-        list_corpus_chords.append(tune_n)
-        list_sectionid.append(sectionid)
-
-
-    _df = pd.DataFrame(list(zip(list_sectionid, list_corpus_chords)),
-                                columns=['sectionid', 'chords'])
-    return _df
-
-
-def plot_weights(doc2vecObj, preprocessing):
-    from sklearn.manifold import TSNE
-    import plotly.express as px
-
-    model = doc2vecObj.doc2vec
-
-    # input data: vectors for all tokens
-    weights = model.wv.vectors
-    chord_names = model.wv.index_to_key
-
-    # only do this when the vocabulary is not too big....
-    if len(weights) > 100:
-        print("Reducing the T-SNE plot to C since the full plot will be too cluttered")
-        # 20 closest chords to 'C'
-        vectors = []
-        chord_names = []
-        similar_chords = model.wv.similar_by_word('C', topn=20)
-        for c in similar_chords:
-            vectors.append(model.wv.get_vector(c[0]))
-            chord_names.append(c[0])
-
-        weights = np.array(vectors)
-
-
-    # do T-SNE
-    tsne = TSNE(n_components=2,
-              random_state=42,
-              perplexity=30,
-              learning_rate='auto',
-              init='pca',
-              n_iter=2000
-              )
-    T = tsne.fit_transform(weights)
-
-    # plot
-    projected = pd.DataFrame(T)
-
-    fig = px.scatter(
-      projected,
-      x=0, y=1,
-      #color='mode',
-      text=chord_names,
-      width=800, height=600,
-      title=f"T-SNE applied to Doc2Vec Weights, {preprocessing}"
-    )
-    fig.update_traces(textposition='top center')
-    fig.update_traces(textfont_size=12, selector=dict(type='scatter'))
-    fig.show()
-
 
 
 def generate_webapp_data(doc2vecObj, preprocessing):
@@ -141,32 +72,18 @@ if __name__ == "__main__":
         wandb = UseWandB(use=False, project_name='doc2vec_dbow1', data=mod, comment="")
         wandb.store_input_file(mod.input_file)
 
-        if False:
+        if True:
             # Calculate the Model
             mod.calculate_doc2vec_model()
             mod.store_model()
 
         mod.load_model()
 
-        diatonic_chords(mod, p)
-
-        df = corpus_chord_ngram(mod, ngrams=[1,2,3,4])
-        #plot_weights(mod, p)
-
-        pairs = ['G7', 'C', 'C7', 'F']
-        pairs = ['G', 'C', 'C', 'F']
-        pairs = ['Am', 'C', 'Cm', 'Eb']
-
-        sims = mod.doc2vec.wv.most_similar(positive=[pairs[1], pairs[2]], negative=[pairs[0]], topn=5)
-        if sims[0][0] == pairs[3]:
-            print(f"Found the analogy: {pairs[0]}-{pairs[1]} is like {pairs[2]}-{pairs[3]}")
-        else:
-            print(f"Did not find the analogy. Best matches are: {sims}")
-
-        mod.doc2vec.wv.evaluate_word_analogies('chords_analogy.txt', case_insensitive=False)
+        # just as a visual cross-check, visualize similar chords
+        similar_chords(mod, p)
 
         # Test
-        #do_contrafacts_test(mod)
+        do_contrafacts_test(mod)
 
         # Generate full data for web application
         if True:
